04525e2d2762cc1e751c43d54f39c87597d76df2
zkils <zkils8302@gmail.com>
2015-12-27 00:35:51 +0900

    db check

--- modules/DBconn.js
+++ modules/DBconn.js
@@ -6,16 +6,68 @@
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var dbinfo = {
     host    :'52.192.93.222',
     port : 3306,
     user : 'root',
     password : 'Lion@123',
     database:'zen_main'
-});
-
-
-exports.getConnection = function(){
-    return connection;
-
 };
+//
+//var connection = mysql.createConnection({
+//    host    :'52.192.93.222',
+//    port : 3306,
+//    user : 'root',
+//    password : 'Lion@123',
+//    database:'zen_main'
+//});
+//
+//exports.setConnection = function(){
+//    console.log("get connection");
+//    connection.connect(function(err) {
+//        if (err) {
+//            console.error('mysql connection error');
+//            console.error(err);
+//            throw err;
+//        }
+//    });
+//};
+//
+//exports.getConnection = function(){
+//    this.setConnection();
+//    return connection;
+//};
+//
+//exports.endConnection = function(){
+//    console.log("end connection");
+//    connection.end(function(err) {
+//        if (err) {
+//            console.error('mysql connection error');
+//            console.error(err);
+//            throw err;
+//        }
+//    });
+//
+//};
+
+exports.doQuery = function(sql, callback){
+    var connection = mysql.createConnection(dbinfo);
+    connection.connect(function(err, res){
+        if(err)
+            return ;
+        else
+            return ;
+    });
+    connection.query(sql, function(err, res, field) {
+        console.log("log - doquery : " + sql);
+        if (err){
+            connection.end();
+            callback({});
+            return;
+        }else {
+            callback(res);
+            return ;
+        }
+    });
+    connection.end();
+}
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,76 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-
-  res.send('get');
-});
 
 router.post('/', function(req,res){
   var userInfo = {'id':req.body.userid, 'password':req.body.password};
-  console.log(req.body);
-  console.log(userInfo);
+  var sqlResult;
+  var tmpRes=res;
+  if(userInfo.id=="" || userInfo.id==null) {
+    console.log("userID:" + userInfo.id + "|");
+    req.flash("info", "put id");
+    res.redirect('back');
+    res.end();
+  }else if(userInfo.password=="" || userInfo.password==null){
+    req.flash("info", "put password");
+    res.redirect('back');
+    res.end();
+  }else{
+    var dbconn = require('../modules/DBconn.js');
+    dbconn.doQuery('select id, password from user where id = "'+userInfo.id+'"',function(rows){
+      console.log(rows);
+     // console.log(this);
+      //tmpRes.json(rows);
 
-  var dbconn = require('../modules/DBconn.js');
-  var connection = dbconn.getConnection();
-  connection.connect(function(err) {
-    if (err) {
-      console.error('mysql connection error');
-      console.error(err);
-      throw err;
-    }
-  });
+      if(rows.length!=0){
+        console.log("input pwd: "+userInfo.password + "\t db pwd: "+rows[0].password +"\t pwd");
+        if(rows[0].password==userInfo.password){
+          tmpRes.redirect("/main");
+        }else{
+          console.log("pwd unmatched");
+          tmpRes.redirect('back');
+        }
 
-  //console.log("get users: "+'select id,AES_DECRYPT(UNHEX(password),'+'\'Lion\''+') from user');
-  var query = connection.query('select id, password from user',function(err,rows){
-    console.log(rows);
-    res.json(rows);
-  });
-  //res.send('respond with a resource :post');
+      }else{
+        tmpRes.redirect('back');
+      }
+
+    });
+    //res.send('respond with a resource ');
+    //
+    //var dbconn = require('../modules/DBconn.js');
+    //var connection = dbconn.getConnection();
+    //console.log("id:"+userInfo.id);
+    //
+    //var query = connection.query('select id, password from user where id = ?',userInfo.id,function(err,rows){
+    //  console.log(rows);
+    //  dbconn.endConnection();
+    //  if(rows.length!=0){
+    //    console.log("input pwd: "+userInfo.password + "\t db pwd: "+rows[0].password +"\t pwd");
+    //    if(rows[0].password==userInfo.password){
+    //      res.redirect("/main");
+    //    }else{
+    //      console.log("pwd unmatched");
+    //      req.flash("warn", "wrong password");
+    //      res.redirect('back');
+    //    }
+    //
+    //  }else{
+    //    req.flash("warn", "wrong id");
+    //    res.redirect('back');
+    //  }
+    //  //res.json(rows);
+    //
+    //});
+
+
+
+    //res.send('respond with a resource :post');
+
+  }
+
+
+
 });
 
 module.exports = router;
