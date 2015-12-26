2c8683a4451feb49a80dac68befe6922faf5f9d6
zkils <zkils8302@gmail.com>
2015-12-26 15:10:54 +0900

    Rejected commit: Default

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '회의실 예약 관리 시스템' });
 });
 
 module.exports = router;
