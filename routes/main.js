df62a73125d96916a0304e6b56bbd0afc894faf7
kimhyemi <jessie8021@gmail.com>
2015-12-27 12:06:01 +0900

    path 수정

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.all('/main', function(req, res, next) {
+router.all('/', function(req, res, next) {
 
     res.render('main', { title: '회의실 예약 관리 시스템'  });
 });
