379df5b3d2ff3e64255338662432ab858dd3a3eb
kimhyemi <jessie8021@gmail.com>
2015-12-28 20:42:57 +0900

    등록, 수정, 삭제 팝업 완료

---
+++ public/script/common.js
@@ -0,0 +1,43 @@
+function init(){
+    if($(".login_wrapper").length === 0) $(document.body).addClass("main");
+    else $(document.body).removeClass("main");
+
+    $(".reservation td > a").on("click",function(){reservation.movePage($(this));});
+    $("#btnUser").on("click", user.display);
+};
+
+
+
+
+var reservation = {
+    data : {
+
+        // DB data binding
+    },
+    movePage : function(obj){
+        var that = obj, _offset = that.offset();
+        console.log(that.offset());
+        // open registReservation
+        //$("#mask").show();
+        //$("#registReservation").show();
+
+        // open modifyReservation
+        $("#btnModify").hide();
+        $("#btnDelete").hide();
+        $("#modifyReservation").css({top : _offset.top + 8, left : _offset.left + 8}).show();
+
+    }
+}
+var user = {
+    data : {},
+    display : function(){
+        $("#mask").show();
+        $("#modifyUser").show();
+    }
+}
+var calendar = {
+    init : function(){
+
+
+    }
+}
