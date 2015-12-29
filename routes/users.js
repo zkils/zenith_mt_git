8921a4a487f714e9fd58ab9090277586b032bf37
zkils <zkils8302@gmail.com>
2015-12-29 19:51:42 +0900

    팀명 변경 쿼리 수정

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,9 @@ var dbconn = require('../modules/DBconn.js');
 router.post('/', function(req,res){
   console.log("user!!");
 
-  var userInfo = {'id':req.session.userid, 'org_password':req.body.org_password, 'new_password':req.body.new_password };
-  var sqlString = 'UPDATE user SET PASSWORD = (HEX(AES_ENCRYPT( ?, ? ))) WHERE ID = ?';
-  var inserts = [userInfo.new_password, 'Lion', userInfo.id];
+  var userInfo = {'id':req.session.userid, 'org_password':req.body.org_password, 'new_password':req.body.new_password, 'new_name':req.body.new_username };
+  var sqlString = 'UPDATE user SET PASSWORD = (HEX(AES_ENCRYPT( ?, ? ))), NAME = ? WHERE ID = ?';
+  var inserts = [userInfo.new_password, 'Lion', userInfo.new_name, userInfo.id];
   var sql = mysql.format(sqlString, inserts);
 
   if(userInfo.new_password==userInfo.org_password){
