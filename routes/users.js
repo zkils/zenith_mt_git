019871302760123978d8b5cfe9de9211dc73b107
zkils <zkils8302@gmail.com>
2015-12-29 20:13:44 +0900

    query. 수정

--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,12 @@ router.post('/', function(req,res){
   var userInfo = {'id':req.session.userid, 'org_password':req.body.org_password, 'new_password':req.body.new_password, 'new_name':req.body.new_username };
   var sqlString = 'UPDATE user SET PASSWORD = (HEX(AES_ENCRYPT( ?, ? ))), NAME = ? WHERE ID = ?';
   var inserts = [userInfo.new_password, 'Lion', userInfo.new_name, userInfo.id];
+  var sql = mysql.format(sqlString, inserts);
 
   // Lion@1234
   if (userInfo.org_password==req.session.password){
     dbconn.pool.getConnection(function(err,connection){
-      var query = connection.query(sqlString, function (err, rows) {
+      var query = connection.query(sql, function (err, rows) {
         if(err){
           connection.release();
           throw err;
