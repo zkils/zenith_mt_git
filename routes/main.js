e58ad7dd0324ec2b0b6f6644337354583c0058ba
zkils <zkils8302@gmail.com>
2016-01-05 09:04:22 +0000

    reservation display when change date

--- routes/main.js
+++ routes/main.js
@@ -42,21 +42,13 @@ function getSchedule(res,req,date){
     }else{
         isToday=false;
     }
-    //TO-DO 쿼리 조인해서 user name / team name 가져올 것
     var sqlString ='SELECT MT_SCHEDULE.ID,MT_DATE,FROM_TIME,TO_TIME,ROOM_ID,USER_ID,MT_SCHEDULE.NAME,PHONE,USER.NAME as USERNAME,ROOM.NAME as ROOMNAME,DATE_FORMAT(MT_SCHEDULE.INSERT_DATE,"%Y-%m-%d %r") as INSERT_DATE FROM MT_SCHEDULE , ROOM , USER WHERE MT_DATE ="'+date+'" and MT_SCHEDULE.ROOM_ID = ROOM.ID and MT_SCHEDULE.USER_ID = USER.ID';
-    //var sqlString = 'SELECT ID,MT_DATE,FROM_TIME,TO_TIME,ROOM_ID,USER_ID,NAME,PHONE,DATE_FORMAT(INSERT_DATE,"%Y-%m-%d %T") as INSERT_DATE FROM MT_SCHEDULE WHERE MT_DATE ="'+date+'"';
-    //console.log(sqlString);
     dbconn.pool.getConnection(function(err,connection){
         var query = connection.query(sqlString, function (err, rows) {
             if(err){
                 connection.release();
                 throw err;
             }
-            //res.send({ username: req.session.username, userid: req.session.userid ,mtSchedule:rows });
-            //res.render('main', {
-            //    rData:rows,
-            //    userInfo:{"username": req.session.username, "userid": req.session.userid}
-            //} );
             if(isToday) {
                 res.render('main', {
                     rData:rows,
@@ -69,8 +61,6 @@ function getSchedule(res,req,date){
                 });
 
             }
-
-            //console.log(rows);
             connection.release();
         });
 
